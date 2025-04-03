5e2bc942ddea6bab0379aa9bc851fc86e5ba9306
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-04-03 15:33:22 +0800

    user interface added

--- flashcard-app/src/main/java/com/flashcard/Main.java
+++ flashcard-app/src/main/java/com/flashcard/Main.java
@@ -1,7 +1,41 @@
 package com.flashcard;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\n****************************");
+        System.out.println(" Welcome to Flashcards CLI! ");
+        System.out.println("****************************\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean running = true;
+
+        while (running) {
+            System.out.println("What do you want to do?");
+            System.out.println("1. Study");
+            System.out.println("2. Edit my collections");
+            System.out.println("3. Quit");
+            System.out.print("Your choice: ");
+
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    running = false;
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.err.println("Invalid choice! Please enter a valid option.");
+            }
+        }
     }
+
 }
