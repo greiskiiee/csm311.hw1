10848d491956673c32abe1eb2a979cfa875b0a8b
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-05-02 12:55:16 +0800

    mistakes fixes

--- flashcard-app/src/main/java/com/flashcard/Flashcard.java
+++ flashcard-app/src/main/java/com/flashcard/Flashcard.java
@@ -5,13 +5,16 @@ public class Flashcard {
     String question;
     String answer;
     String help;
-    int mistakes;
+    boolean[] mistakes;
 
     public Flashcard(String question, String answer, String help) {
         this.question = question;
         this.answer = answer;
         this.help = help;
-        this.mistakes = 0;
+    }
+
+    public void initializeMistakes(int repetition) {
+        this.mistakes = new boolean[repetition];
     }
 
     public String getQuestion() {
@@ -26,11 +29,32 @@ public class Flashcard {
         return help;
     }
 
-    public int getMistakes() {
-        return mistakes;
+    public int getTotalMistakes() {
+        int count = 0;
+        if (this.mistakes != null) {
+            for (boolean m : this.mistakes) {
+                if (m) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
-    public void incrementMistakes() {
-        this.mistakes++;
+    public boolean[] getMistakes() {
+        return this.mistakes;
+    }
+
+    public void markMistake(int repetitionIndex, boolean madeMistake) {
+        if (this.mistakes != null && repetitionIndex >= 0 && repetitionIndex < mistakes.length) {
+            this.mistakes[repetitionIndex] = madeMistake;
+        }
+    }
+
+    public boolean madeRecentMistake() {
+        if (this.mistakes != null && this.mistakes.length > 0) {
+            return this.mistakes[mistakes.length - 1];
+        }
+        return false;
     }
 }
